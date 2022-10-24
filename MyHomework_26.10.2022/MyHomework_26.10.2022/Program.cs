3c16003d3b3976952d95a13d86be0996bf1093a8
Ivan <v.gritskevich25@gmail.com>
2022-10-25 01:52:31 +0300

    added console proj

---
+++ MyHomework_26.10.2022/MyHomework_26.10.2022/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MyHomework_26._10._2022
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
