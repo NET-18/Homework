a037215380e91e2503b101521c7d95517c3bc151
Sergey <serge.juskevic@gmail.com>
2022-10-25 11:15:59 +0300

    Added homework 26.10.2022

---
+++ HomeWork_26.10.2022/HomeWork_26.10.2022/Point3D.cs
@@ -0,0 +1,15 @@
+namespace HM_26_10_2022;
+
+public struct Point3D
+{
+    public double X { get; set; }
+    public double Y { get; set; }
+    public double Z { get; set; }
+
+    public Point3D(double x, double y, double z)
+    {
+        this.X = x;
+        this.Y = y;
+        this.Z = z;
+    }
+}
