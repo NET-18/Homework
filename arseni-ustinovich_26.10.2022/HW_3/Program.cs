6c2d3dc217335427490b2c14f0394b1bdabe7eb8
Simon <arseni.ustinovich@gmail.com>
2022-10-25 14:08:30 +0300

    less 2 hw

---
+++ arseni-ustinovich_26.10.2022/HW_3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Xml.Linq;
+
+namespace HW_3
+{
+    public class Person
+    {
+        public int _age;
+        public string _name;
+        public string _surname;
+
+        public Person(int age, string name, string surname)
+        {
+            this._age = age;
+            this._name = name;
+            this._surname = surname;
+        }
+
+        public Person()
+        {
+            this._age = 15;
+            this._name = "Boleslava";
+            this._surname = "Joash";
+        }
+    }
+    
+    internal class Program
+    {
+        
+        static void MyOutput(Person A)
+        {
+            Console.WriteLine("{0} {1} is {2} years old", A._surname, A._name, A._age);
+        }
+
+        static void Main(string[] args)
+        {
+            Person master = new Person(99, "Vasiliy", "Pupkin");
+            MyOutput(master);
+
+            master = new Person();
+            MyOutput(master);
+        }
+    }
+}
---
+++ arseni-ustinovich_26.10.2022/HW_4/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace HW_4
+{  
+   
+    internal class Program
+    {
+        public struct XYZ
+        {
+            public double _x;
+            public double _y;
+            public double _z;
+
+            public XYZ(double x, double y, double z)
+            {
+                this._x = x;
+                this._y = y;
+                this._z = z;
+            }
+
+        }
+        
+        static double distance(XYZ a, XYZ b) => Math.Sqrt(Math.Pow(a._x - b._x, 2) + Math.Pow(a._y - b._y, 2) + Math.Pow(a._z - b._z, 2));
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter A coordinates:");
+            XYZ A = new XYZ(Convert.ToDouble(Console.ReadLine()), Convert.ToDouble(Console.ReadLine()), Convert.ToDouble(Console.ReadLine()));
+
+            Console.WriteLine("Enter B coordinates:");
+            XYZ B = new XYZ(Convert.ToDouble(Console.ReadLine()), Convert.ToDouble(Console.ReadLine()), Convert.ToDouble(Console.ReadLine()));
+
+            Console.WriteLine("Distannce between A and B is {0:f4}", distance(A, B));
+        }
+    }
+}
