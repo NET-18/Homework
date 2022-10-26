9408032eac890ce29cf9c3deb5624dc2da9cf56a
Lyubov <lyubaknyaz@gmail.com>
2022-10-26 16:18:17 +0300

    homework2 fixes

--- HomeWork2/HomeWork2/Program.cs
+++ HomeWork2/HomeWork2/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Xml.Linq;
+﻿using System;
+using System.Xml.Linq;
 
 namespace HomeWork2
 {
@@ -8,14 +9,18 @@ namespace HomeWork2
         public string? name;
         public string? surname;
 
+        public Person()
+        {
+
+        }
+
         public Person(int a1, string? a2, string? a3)
         {
             age = a1;
             name = a2;
             surname = a3;
         }
-
-
+        public override string ToString() => $"{name} {surname} is {age} years old";
     }
 
     public struct Point
@@ -31,48 +36,35 @@ namespace HomeWork2
             this.z = z;
         }
 
-        
+        public override string ToString() => $"{{X={x}, Y={y}, Z={z}}}";
+
     }
 
     internal class Program
     {
         public static void Print(Person person)
         {
-            Console.WriteLine($"{person.name} {person.surname} is {person.age} years old");
+            Console.WriteLine(person);
         }
         
-        
-        public double x;
-        public double y;
-        public double z;
-
-        public Program(double x, double y, double z)
-        {
-            this.x = x;
-            this.y = y;
-            this.z = z;
-        }
-        public static double Distance(Program a, Program b)
+              
+        public static double Distance(Point a, Point b)
         {
             double dX = a.x - b.x;
             double dY = a.y - b.y;
             double dZ = a.z - b.z;
             return Math.Sqrt(dX * dX + dY * dY + dZ * dZ);
         }
-
-        public override string ToString() => $"{{X={x}, Y={y}, Z={z}}}";
-
-        public double Distance(Program otherPoint) => Distance(this, otherPoint);
-
+        
         static void Main(string[] args)
         {
             Person person = new(23, "Vivienne", "Storm");
             Print(person);
 
-            Program point1 = new Program(3, 7, 16);
-            Program point2 = new Program(7, 3, 5);
+            Point point1 = new Point(3, 7, 16);
+            Point point2 = new Point(7, 3, 5);
 
-            Console.WriteLine($"Расстояние от точки {point1} до точки {point2} равно {point1.Distance(point2)}");
+            Console.WriteLine($"Расстояние от точки {point1} до точки {point2} равно {Distance(point1,point2)}");
 
             Console.ReadKey();
         }
