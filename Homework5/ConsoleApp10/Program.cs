0ce81213d3edf4deb228ae23329da14b605ad9de
DmitryMurav <Dmitrymuravev11@gmail.com>
2022-10-29 18:04:25 +0300

    My homeworkqwe

---
+++ Homework5/ConsoleApp10/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Xml.Linq;
+
+namespace ConsoleApp10
+{
+    internal class Program
+    {
+        public class Person
+        {
+            public string _name;
+            public string _surname;
+            public int _age;
+        }
+        public class Student
+        {
+            public Person person;
+            public string _university;
+            public string _department;
+            public int _course;
+        }
+        public class Employee
+        {
+            public Person person1;
+            public string _job;
+            public string _salary;
+            public string _exp;
+        }
+        static void Main(string[] args)
+        {
+            Person p = new Person()
+            {
+                _name = "name",
+                _surname = "surname",
+                _age = 18,
+            };
+            Student s = new Student()
+            {
+                person = p,
+                _university = "university",
+                _department = "department",
+                _course = 4,
+            };
+            Employee e = new Employee()
+            {
+                person1 = p,
+                _job = "job",
+                _salary="salary",
+                _exp="exp",
+            };
+            Console.WriteLine($"{p._name} {p._surname} is {p._age} years old.");
+            Console.WriteLine($"{p._name} {p._surname} is study at {s._university} on {s._department} in {s._course}.");
+            Console.WriteLine($"{p._name} {p._surname} have a {e._job} with big {e._exp} and good {e._salary}.");
+        }
+    }
+}
---
+++ Homework6/ConsoleApp13/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+
+namespace ConsoleApp13
+{
+    internal class Program
+    {
+        class Person
+        {
+            public string _name;
+            public string _surname;
+            public int _age;
+        }
+        class Student
+        {
+            public string _university;
+            public string _department;
+            public int _course;
+            public Person person1;
+        }
+        class Employee
+        {
+            public string _job;
+            public int _salary;
+            public int _exp;
+            public Person person2;
+        }
+        static void Personinfo(Person person)
+        {
+            Console.WriteLine($"{person._name} {person._surname} is {person._age} years old");
+        }
+        static void StudentInfo(Student student)
+        {
+            Console.WriteLine();
+            Personinfo(student.person1);
+            Console.WriteLine($"He is study at {student._university} in {student._department} on {student._course}");
+
+        }
+        static void EmployeeInfo(Employee employee)
+        {
+            Console.WriteLine();
+            Personinfo(employee.person2);
+            Console.WriteLine($"{employee.person2._name} has {employee._exp} years experience. Make his {employee._job} very well and got a salary {employee._salary}");
+
+        }
+        static void salary(Employee employee)
+        {
+            Console.WriteLine();
+            Console.WriteLine($"Зарплата сотрудника {employee._salary} долларов");
+            Console.WriteLine("На сколько % увеличить");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int result = 1000 * a / 100;
+            if (a > 0 && a < 100)
+            {
+                Console.Write($"Хорошо, прибавка к зарплате: {result} ");
+                Console.WriteLine(" долларов");
+            }
+            else
+            {
+                Console.WriteLine("Так не бывает");
+            }
+        }
+        static void StudentDeduction(Student student)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Отчисляем студента");
+            StudentInfo(student);
+            Console.WriteLine("Отчислен с позором");
+        }
+        static void EmployeeDefited(Employee employee)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Увольняем и точка.");
+            EmployeeInfo(employee);
+            Console.WriteLine("Уволен и забыт");
+        }
+        static void Main(string[] args)
+        {
+            Person p = new()
+            {
+                _age = 18,
+                _name = "Dmitry",
+                _surname = "Mur",
+            };
+            Student s = new()
+            {
+                person1 = new()
+                {
+                    _age=20,
+                    _name="Ivan",
+                    _surname="git",
+                },
+                _course = 4,
+                _department = "Optic",
+                _university = "BNTU",
+
+            };
+            Employee d = new()
+            {
+                person2 = new()
+                {
+                    _age=33,
+                    _name="Petya",
+                    _surname="Goroh",
+                },
+                _exp = 3,
+                _job = "pisyn",
+                _salary = 1000,
+            };
+            Personinfo(p);
+            StudentInfo(s);
+            EmployeeInfo(d);
+            salary(d);
+            StudentDeduction(s);
+            EmployeeDefited(d);
+        }
+    }
+}
