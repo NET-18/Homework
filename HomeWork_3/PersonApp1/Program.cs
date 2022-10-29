a27c67a43536a922afeb979124f491e7bb1684d2
Bobko <bobko.alex3@gmail.com>
2022-10-29 10:51:31 +0300

    latest corrected version

--- HomeWork_3/PersonApp1/Program.cs
+++ HomeWork_3/PersonApp1/Program.cs
@@ -3,8 +3,22 @@
     public class Person
     {
         public string _name;
-        public string _surName;
+        public string _surname;
         public int _age;
+
+
+        public Person()
+        {
+            _name = "Александр";
+            _surname = "Бобко";
+            _age = 14;
+        }
+        public Person(string name, string surname, int age)
+        {
+            this._name = name;
+            this._surname = surname;
+            this._age = age;
+        }
 
 
     }
@@ -13,25 +27,14 @@
 
         static void Main(string[] args)
         {
-            Person a = new Person();
-            Console.Write("Введите свою фамилию: ");
-            a._surName = Convert.ToString(Console.ReadLine());
-
-
-            Console.Write("Введите свое имя: ");
-            a._name = Convert.ToString(Console.ReadLine());
-
-            Console.Write("Введите свой возраст: ");
-            a._age = Convert.ToInt32(Console.ReadLine());
-
+            Person a = new Person( "Александр", "Бобко", 24);
             OutPut(a);
-
 
         }
         static void OutPut(Person t)
         {
 
-            Console.Write($"Вы {t._surName} {t._name} и вам {t._age} лет");
+            Console.Write($"Вы {t._surname} {t._name} и вам {t._age} лет");
         }
     }
 }
--- HomeWork_4/DistanceApp/Program.cs
+++ HomeWork_4/DistanceApp/Program.cs
@@ -1,45 +1,43 @@
 ﻿namespace DistanceApp
 {
-   
+
     internal class Program
     {
-        struct Solid
+        public struct Solid
         {
-            public Double x_1;
-            public Double x_2;
-            public Double y_1;
-            public Double y_2;
-            public Double z_1;
-            public Double z_2;
+            public Double x;
+            public Double y;
+            public Double z;
         }
-        static void Main(string[] args)
+        static void Main()
         {
-            Solid a = new Solid();
+            Solid a1 = new Solid();
+            Solid a2 = new Solid();
             Console.Write("Введите переменную х для точки А: ");
-            a.x_1 = Convert.ToDouble(Console.ReadLine());
+            a1.x = Convert.ToDouble(Console.ReadLine());
 
             Console.Write("Введите переменную y для точки А: ");
-            a.y_1 = Convert.ToDouble(Console.ReadLine());
+            a1.y = Convert.ToDouble(Console.ReadLine());
 
             Console.Write("Введите переменную z для точки А: ");
-            a.z_1 = Convert.ToDouble(Console.ReadLine());
+            a1.z = Convert.ToDouble(Console.ReadLine());
 
             Console.Write("Введите переменную х для точки Б: ");
-            a.x_2 = Convert.ToDouble(Console.ReadLine());
+            a2.x = Convert.ToDouble(Console.ReadLine());
 
             Console.Write("Введите переменную y для точки Б: ");
-            a.y_2 = Convert.ToDouble(Console.ReadLine());
+            a2.y = Convert.ToDouble(Console.ReadLine());
 
             Console.Write("Введите переменную z для точки Б: ");
-            a.z_2 = Convert.ToDouble(Console.ReadLine());
+            a2.z = Convert.ToDouble(Console.ReadLine());
 
-            EnterValue(a.x_1, a.y_1, a.z_1, a.x_2, a.y_2, a.z_2);
+            EnterValue(a1, a2);
         }
 
-        static void EnterValue(double x_1, double y_1, double z_1, double x_2, double y_2, double z_2)
+        static void EnterValue(Solid a1, Solid a2)
         {
-            
-            double result = Math.Sqrt(Math.Pow(x_2 - x_1, 2) + Math.Pow(y_2 - y_1, 2) + Math.Pow(z_2 - z_1, 2));
+
+            double result = Math.Sqrt(Math.Pow(a2.x - a1.x, 2) + Math.Pow(a2.y - a1.y, 2) + Math.Pow(a2.z - a1.z, 2));
             Console.Write(result);
 
 
