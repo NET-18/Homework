9e392c61ae2d2604bda922f205eda0b36a3c5312
usernameNIK <nik.lishhuk@mail.ru>
2022-11-01 01:11:59 +0300

    Add test2

---
+++ HomeWork2/test2/Point.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test2
+{
+    internal class Point
+    {
+        public double x != null; 
+        public double y;
+        public double z;
+        public double d; 
+    }
+}
---
+++ HomeWork2/test2/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace test2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Point x = new Point();
+            Point x1 = new Point();
+            Point y = new Point();
+            Point y1 = new Point();
+            Point z = new Point();
+            Point z1 = new Point();
+
+            Point distance = new Point();
+            distance = Math.Sqrt(Math.Pow(x1 - x, 2) + Math.Pow(y1 - y, 2) + Math.Pow(z1 - z, 2));
+            Console.WriteLine(distance.ToString()); 
+        }
+    }
+}
