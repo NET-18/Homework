cb02f05caf6826bf493c76eb37634467883d602c
Daniil <KantaiCommander@outlook.com>
2022-10-31 20:49:20 +0300

    homework on 24.10

---
+++ 24.10.2022/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace _24._10._2022
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter next number");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Summury = {Summury}, Subtraction = {Subtraction}");
+            Console.WriteLine($"Multiplication  = {Multiplication}, Division = {Division}");
+
+
+            static int Summury(int a, int b);
+            { 
+                int sum = a + b; 
+            }
+
+            static int Subtraction(int a, int b);
+            {
+                int sub = a - b;
+            }
+            static int Multiplication(int a, int b);
+            {
+                int mult = a * b;
+            }
+            static int Division(int a, int b);
+            {
+                int div = a / b;
+            }
+
+        }
+
+    }
+}
