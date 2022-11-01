8fd619170f7b469950fde60f0e903a59d8b5ad9a
DmitryMurav <Dmitrymuravev11@gmail.com>
2022-11-01 19:38:14 +0300

    Corrected homework

---
+++ ConsoleApp6/ConsoleApp6/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace ConsoleApp6
+{
+    internal class Program
+    {
+        public class Person
+        {
+            public string _name;
+            public string _surname;
+            public int _age;
+            public Person()
+            {
+                _name = "pip";
+                _surname = "Mur";
+                _age = 14;
+            }
+            public Person(string name, string surname, int age)
+            {
+                _name = name;
+                _surname = surname;
+                _age = age;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Person josh = new Person("Josh", "Baleslava", 15);    
+            Print(josh);
+        }
+        static void Print(Person person)
+        {
+            Console.WriteLine($"{person._surname} {person._name} is {person._age} years old");
+        }
+    }
+}
