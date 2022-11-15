bc431055af657932cac4ef76243bb3ae9ca3b6f6
Andrey_m4k <andrey.mar4uk2011@yandex.ru>
2022-11-15 22:34:49 +0300

    next commit, changes by comments

--- Program.cs
+++ Program.cs
@@ -13,86 +13,86 @@
     {
         void Move(int x, int y);
 
-        Point Point { set; get; }
+        Point point { set; get; }
     }
     public class Person : IMovable
     {
-        Point Points = new Point();
+        Point points = new Point();
 
         public void Move(int x, int y)
         {
-            Points.x = x;
-            Points.y = y;
+            points.x = x;
+            points.y = y;
         }
-        public Point Point
+        public Point point
         {
             get
             {
-                return Points;
+                return points;
             }
             set
             {
-                Points = value;
+                points = value;
             }
         }
     }
     public class Bike : IMovable
     {
-        Point Points = new Point();
+        Point points = new Point();
 
         public void Move(int x, int y)
         {
-            Points.x = x;
-            Points.y = y;
+            points.x = x;
+            points.y = y;
         }
-        public Point Point
+        public Point point
         {
             get
             {
-                return Points;
+                return points;
             }
             set
             {
-                Points = value;
+                points = value;
             }
         }
 
     }
     public class Car : IMovable
     {
-        Point Points = new Point();
-        public Point Point
+        Point points = new Point();
+        public Point point
         {
             get
             {
-                return Points;
+                return points;
             }
             set
             {
-                Points = value;
+                points = value;
             }
         }
         public void Move(int x, int y)
         {
-            Points.x = x;
-            Points.y = y;
+            points.x = x;
+            points.y = y;
         }
     }
     public static class Calculations
     {
         public static void Delta(IMovable Movable1, IMovable Movable2)
         {
-            double d = Math.Sqrt(Math.Pow((Movable1.Point.x - Movable2.Point.x), 2) + Math.Pow((Movable1.Point.y - Movable2.Point.y), 2));
+            double d = Math.Sqrt(Math.Pow((Movable1.point.x - Movable2.point.x), 2) + Math.Pow((Movable1.point.y - Movable2.point.y), 2));
 
             Console.WriteLine($"Delta between Movable1 and Movable2 is {d}");
         }
         public static void Move1To2(IMovable Movable1, IMovable Movable2)
         {
-            Movable1.Move(Movable2.Point.x, Movable2.Point.y);
+            Movable1.Move(Movable2.point.x, Movable2.point.y);
         }
         public static void Move2To1(IMovable Movable1, IMovable Movable2)
         {
-            Movable2.Move(Movable1.Point.x, Movable1.Point.y);
+            Movable2.Move(Movable1.point.x, Movable1.point.y);
         }
     }
     public class Program
@@ -102,18 +102,19 @@
             Person Ivan = new Person();
 
             Ivan.Move(5, 6);
-            Console.WriteLine(Ivan.Point.x);
+            Console.WriteLine(Ivan.point.x);
 
             Car Lambada = new Car();
 
             Lambada.Move(16, 6);
-            Console.WriteLine($"Lambada.x ={Lambada.Point.x}");
-            Console.WriteLine($"Lambada.y ={Lambada.Point.y}");
+            Console.WriteLine($"Lambada.x ={Lambada.point.x}");
+            Console.WriteLine($"Lambada.y ={Lambada.point.y}");
 
             Calculations.Delta(Lambada, Ivan);
             Calculations.Move1To2(Lambada, Ivan);
-            Console.WriteLine($"Lambada.x ={Lambada.Point.x}");
-            Console.WriteLine($"Lambada.y ={Lambada.Point.y}");
+
+            Console.WriteLine($"Lambada.x ={Lambada.point.x}");
+            Console.WriteLine($"Lambada.y ={Lambada.point.y}");
 
         }
 
