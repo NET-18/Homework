0cdcc2b1bc87defbc82548d95a3f3c7e4a2857ad
Andrey_m4k <andrey.mar4uk2011@yandex.ru>
2022-11-20 22:32:23 +0300

    first commit

---
+++ MyCollection.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+
+namespace ConsoleApp4
+{
+    internal class MyCollection : IEnumerable<int>
+    {
+        private int[] data = new int[] { 1, 2, 3, 4, 5, 6, 7 };
+
+        public IEnumerator<int> GetEnumerator() 
+            => new MyEnumerator(data);
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        System.Collections.Generic.IEnumerator<int> IEnumerable<int>.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        System.Collections.IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        private class MyEnumerator : IEnumerator<int>
+        {
+            private readonly int[] data;
+            private int index = -1;
+
+            public object Current
+            {
+                get
+                {
+                    if (index == -1 || index >= data.Length)
+                    {
+                        throw new ArgumentException();
+                    }
+
+                    return data[index];
+                }
+            }
+
+            object IEnumerator.Current => this.Current;
+
+            public MyEnumerator(int[] data)
+            {
+                this.data = data;
+            }
+            public bool MoveNext()
+            {
+                if (index < data.Length - 1)
+                {
+                    index++;
+                    return true;
+                }
+                return false;
+            }
+
+            public void Reset()
+            {
+                index = -1;
+            }
+
+            public void Dispose()
+            {
+
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ConsoleApp4
+{
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var myCollection = new List<int>()
+            {
+                1, 2, 3, 4, 5, 6, 7
+            };
+            myCollection.Add(1);
+
+            var myCollection = new MyCollection();
+            foreach (var item in myCollection)
+            {
+                Console.WriteLine(item);
+            }
+        }  
+
+    }
+}
