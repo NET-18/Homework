47b4d3791df92cbebaf3b1fd73bbc9a4c2094d49
vadim <vadim.presman@gmail.com>
2022-11-20 16:12:19 +0300

    added my homework

---
+++ TMS_Seventh_App/MyList.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMS_Seventh_App
+{
+    internal class MyList : IEnumerable
+    {
+        private int[] data = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        public IEnumerator GetEnumerator() => new MyEnumirator(data);
+
+        private class MyEnumirator : IEnumerator
+        {
+            private readonly int[] data;
+            private int index = -1;
+
+            public object Current
+            {
+                get
+                {
+                    if(index == -1 || index >= data.Length)
+                    {
+                        throw new ArgumentException();
+                    }
+
+                    return data[index];
+                }     
+            }
+
+            public MyEnumirator(int[] data)
+            {
+                this.data = data;
+            }
+
+
+            public bool MoveNext()
+            {
+                if(index < data.Length-1)
+                {
+                    index++;
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            public void Reset()
+            {
+                index= -1;
+            }
+        }
+    }
+}
---
+++ TMS_Seventh_App/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace TMS_Seventh_App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var myList = new MyList();
+            foreach (var item in myList)
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.WriteLine("\n");
+
+            var newList = new List<MyNewClass>()
+            {
+                new MyNewClass(1),
+                new MyNewClass(4),
+                new MyNewClass(10),
+                new MyNewClass(6),
+                new MyNewClass(7)
+            };
+
+            foreach (var item in newList)
+            {
+                Console.WriteLine(item.Number);
+            }
+
+            newList.Sort();
+            Console.WriteLine();
+
+            foreach (var item in newList)
+            {
+                Console.WriteLine(item.Number);
+            }
+        }
+    }
+}
