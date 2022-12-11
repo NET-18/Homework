9a6bf18b823810de3c3cbc97a9a4b61584df4777
Andrey_m4k <andrey.mar4uk2011@yandex.ru>
2022-12-11 19:47:55 +0300

    Andrew_Marchuk_Homework_14.12.2022

---
+++ Easy_Type/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace ConsoleApp6
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    
+    class Program
+    {
+        public static async Task SimpleWriteAsync(List<Task<string>> tasks)
+        {
+            Console.WriteLine("SimpleWriteAsync Start");
+            string filePath = "RandomWords.txt";
+            string text = "";
+            foreach (var item in tasks)
+            {
+                  text += item.Result + "\n\n";
+            }
+            await File.WriteAllTextAsync(filePath, text);
+            Console.WriteLine("SimpleWriteAsync End");
+        }
+        public static string StringRandom(int v, int j)
+        {
+            //Console.WriteLine("StringRandom Start");
+            var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            var stringChars = new char[v];
+            var random = new Random();
+            for (int i = 0; i < v; i++)
+            {
+                stringChars[i] = chars[random.Next(chars.Length)];
+            }
+            Task.Delay(random.Next(100)).Wait();
+            var s = new string(stringChars);
+            Console.Write($"{j},\t");
+            return s;
+        }
+        static void Main(string[] args)
+        {
+            int v = 1000;
+            int j=0;
+            List<Task<string>> tasks = new List<Task<string>>(v);
+            
+            for (int i = 0; i <= v; i++)
+            {
+                tasks.Add(Task.Factory.StartNew(() => StringRandom(v, j++)));
+            }
+
+            Task.WhenAll(tasks).Wait();
+            SimpleWriteAsync(tasks).Wait();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Hard_Type/Program.cs
@@ -0,0 +1,61 @@
+﻿using System.Net;
+using System.Text;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        public static async Task SimpleWriteAsync(List<Task<string>> tasks)
+        {
+            Console.WriteLine("SimpleWriteAsync Start");
+            string filePath = "RandomSites.txt";
+            string text = "";
+            foreach (var item in tasks)
+            {
+                text += item.Result + "\n\n";
+            }
+            await File.WriteAllTextAsync(filePath, text);
+            Console.WriteLine("SimpleWriteAsync End");
+        }
+        
+        public static string DownloadWebsite(string websiteURL, int j)
+        {
+            WebsiteDataModel output = new WebsiteDataModel();
+            WebClient client = new WebClient();
+
+            output.Url = websiteURL;
+            output.Data = client.DownloadString(websiteURL);
+            Console.WriteLine(j);
+            return output.Data;
+        }
+        
+
+        static void Main(string[] args)
+        {
+            int j = 0;
+            
+            List<Task<string>> tasks = new List<Task<string>>();
+
+            string[] sites = new string[]
+            {
+                "https://www.yahoo.com",
+                "https://www.google.com",
+                "https://www.microsoft.com",
+                "https://www.cnn.com",
+                "https://www.codeproject.com",
+                "https://www.cdprojektred.com"
+            };
+            
+            for (int i = 0; i < sites.Length-1; i++)
+            {
+                tasks.Add(Task.Factory.StartNew(() => DownloadWebsite(sites[i],j++)));
+            }
+
+            Task.WhenAll(tasks).Wait();
+            SimpleWriteAsync(tasks).Wait();
+            Console.ReadLine();
+
+
+        }
+    }
+}
