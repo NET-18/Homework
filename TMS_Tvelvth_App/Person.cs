bd985c1d4f5bd036d5092e803f373e16c0ac3e22
vadim <vadim.presman@gmail.com>
2022-12-18 22:34:41 +0300

    vadim-presman_19.12.2022

---
+++ TMS_Tvelvth_App/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMS_Tvelvth_App
+{
+    internal class Person
+    {
+        public string Id { get; set; }
+        public string Guid { get; set; }
+        public bool IsActive { get; set; }
+        public string Balance { get; set; }
+        public int Age { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        public string About { get; set; }
+        public string[] Tags { get; set; }
+        public List<Friends> Friends { get; set; }
+    }
+}
