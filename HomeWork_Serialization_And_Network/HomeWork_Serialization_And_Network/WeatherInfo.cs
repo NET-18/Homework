c1616e651ba604c34c7171652336240bd69879fc
Sergey <serge.juskevic@gmail.com>
2022-12-26 10:50:59 +0300

    Complete homework

---
+++ HomeWork_Serialization_And_Network/HomeWork_Serialization_And_Network/WeatherInfo.cs
@@ -0,0 +1,13 @@
+namespace HomeWork_Serialization_And_Network;
+
+public class WeatherInfo
+{
+    public int Id { get; set; }
+    public DateTime Date { get; set; }
+    public string Slug { get; set; }
+    public string Type { get; set; }
+    public string Link { get; set; }
+    public Excerpt Excerpt { get; set; }
+    public int Author { get; set; }
+    
+}
