619bcd4fe3f8824e433802419407898d9189a838
Andrey_m4k <andrey.mar4uk2011@yandex.ru>
2023-01-04 00:10:08 +0300

    Andrew_Marchuk_26-28.12.2022

---
+++ WebApplication2/Seed.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using WebApplication2.Services;
+
+namespace WebApplication2
+{
+    public static class Seed
+    {
+        public static async Task SeedForecasts()
+        {
+            if (File.Exists("data.json"))
+            {
+                return;
+            }
+            var list = new List<WeatherForecast>();
+
+            for (int i = 0; i < 100; i++)
+            {
+                var temp = Random.Shared.Next(-15, 30);
+                list.Add(new WeatherForecast()
+                {
+                    Id = i + 1,
+                    Date = DateTime.Now,
+                    Summary = temp < 5 ? "Cold" : "Norm",
+                    TemperatureC = temp,
+                }); ;
+            }
+
+            var json = JsonConvert.SerializeObject(list);
+            await File.WriteAllTextAsync("data.json", json);
+        }
+        
+    }
+}
---
+++ WebApplication2/Services/WeatherForecastService.cs
@@ -0,0 +1,72 @@
+﻿using Newtonsoft.Json;
+
+namespace WebApplication2.Services
+{
+    public class WeatherForecastService
+    {
+        private readonly SomeScopedService _someScopedService;
+
+        public WeatherForecastService(SomeScopedService someScopedService)
+        {
+            _someScopedService = someScopedService;
+        }
+
+        public async Task<List<WeatherForecast>> GetAllAsync()
+        {
+            Console.WriteLine($"service: {_someScopedService.Id}");
+
+            var json = await File.ReadAllTextAsync("data.json");
+            return JsonConvert.DeserializeObject<List<WeatherForecast>>(json);
+        }
+
+        public async Task<WeatherForecast> GetByIdAsync(int id)
+        {
+            var json = await File.ReadAllTextAsync("data.Json");
+            return JsonConvert.DeserializeObject<List<WeatherForecast>>(json)
+               .FirstOrDefault(f => f.Id == id);
+        }
+        
+        public async Task<WeatherForecast> GetRefreshAsync(int id, int TempC, string summary)
+        {
+            var json = await File.ReadAllTextAsync("data.Json");
+            var list = JsonConvert.DeserializeObject<List<WeatherForecast>>(json);
+
+            list[id-1].TemperatureC = TempC;
+            list[id-1].Summary = summary;
+
+            var jsonResult = JsonConvert.SerializeObject(list);
+            await File.WriteAllTextAsync("data.json", jsonResult);
+            return JsonConvert.DeserializeObject<List<WeatherForecast>>(jsonResult)
+               .FirstOrDefault(f => f.Id == id);
+        }
+        
+        public async Task DeleteByIdAsync(int id)
+        {
+            var json = await File.ReadAllTextAsync("data.Json");
+            var list = JsonConvert.DeserializeObject<List<WeatherForecast>>(json);
+            list.RemoveAt(id+1);
+            var jsonResult = JsonConvert.SerializeObject(list);
+            await File.WriteAllTextAsync("data.json", jsonResult);
+        }
+
+        public async Task<WeatherForecast> GetAddAsync()
+        {
+            var json = await File.ReadAllTextAsync("data.json");
+            var list = JsonConvert.DeserializeObject<List<WeatherForecast>>(json);
+            var temp = Random.Shared.Next(-15, 30);
+            list.Add(new WeatherForecast()
+            {
+                Id = list.Count + 1,
+                Date = DateTime.Now,
+                Summary = temp < 5 ? "Cold" : "Norm",
+                TemperatureC = temp,
+            });
+            var jsonResult = JsonConvert.SerializeObject(list);
+            await File.WriteAllTextAsync("data.json", jsonResult);
+            return JsonConvert.DeserializeObject<List<WeatherForecast>>(jsonResult)
+               .Last();
+        }
+
+
+    }
+}
