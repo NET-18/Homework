7588c9f646c83b1eaf77d1c70b5a495775736135
Andrey_m4k <andrey.mar4uk2011@yandex.ru>
2023-01-13 22:42:33 +0300

    Homework

---
+++ WebApplication3/Controllers/GenerateController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace WebApplication3.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class GenerateController : Controller
+    {
+        private readonly GenerateSevices _someScopedService;
+
+        private readonly ILogger<GenerateController> _logger;
+
+        public GenerateController(ILogger<GenerateController> logger, GenerateSevices someScopedService)
+        {
+            _logger = logger;
+            _someScopedService = someScopedService;
+        }
+
+       
+        [HttpGet]
+        public async Task<ActionResult<Person>> GetRandom()
+        {
+            var rand = new Random();
+            int friends = rand.Next(0,10);
+            int tags = rand.Next(2, 8);
+            return await _someScopedService.GetRandomPerson(friends, tags);
+        }
+        [HttpGet("count/{count:int}/friends/{friends:int}/tags/{tags:int}/")]
+        public async Task<ActionResult<List<Person>>> GetById(int count, int friends, int tags)
+        {
+            var list = new List<Person>();
+
+            for (int i = 0; i < count; i++)
+            {
+                list.Add(await _someScopedService.GetRandomPerson(friends, tags));
+            }
+            var jsonResult = JsonConvert.SerializeObject(list);
+            
+            return JsonConvert.DeserializeObject<List<Person>>(jsonResult);
+            
+        }
+    }
+}
---
+++ WebApplication3/Friend.cs
@@ -0,0 +1,18 @@
+﻿namespace WebApplication3
+{
+    public class Friend
+    {
+        public Friend()
+        {
+        }
+
+        public Friend(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
