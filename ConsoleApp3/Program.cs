9dc581e4a0998b028c99e452f88a8a90fab62b66
Andrey_m4k <andrey.mar4uk2011@yandex.ru>
2023-01-18 18:36:26 +0300

    Andrey_marchuk_18.01.2023

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static string connectionString = @"Data Source=DESKTOP-KE0BFAK\SQLEXPRESS;Initial Catalog=TMS_04_01_2023;Integrated Security=True;TrustServerCertificate=true";
+
+        static async Task Main(string[] args)
+        {
+           using SqlConnection conn = new SqlConnection(connectionString);
+            
+            var list = new List<SqlParameter>();
+
+            list.Add(new SqlParameter("@a8", "A8"));
+            list.Add(new SqlParameter("@a6", "A6"));
+            list.Add(new SqlParameter("@a5", "A5"));
+            list.Add(new SqlParameter("@a4", "A4"));
+            try
+            {
+                conn.Open();
+                
+                foreach (var item in list)
+                {
+                    var sql = $"INSERT INTO Models ([name], manufacturerId) VALUES ({item.ParameterName}, 3);";
+                    var command = new SqlCommand(sql, conn);
+                    command.Parameters.Add(item);
+                  
+                    command.ExecuteNonQuery();
+                    command.Dispose();
+                }
+                
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            var adapter = new SqlDataAdapter("SELECT * FROM  [Models]", conn);
+            var set = new DataSet();
+
+            adapter.Fill(set);
+
+            var table = set.Tables[0];
+
+            foreach (DataColumn col in table.Columns)
+            {
+                Console.Write("{0,15}", col.ColumnName);
+            }
+            Console.WriteLine();
+
+            foreach (DataRow row in table.Rows)
+            {
+                foreach (var cell in row.ItemArray)
+                {
+                    Console.Write("{0,15}", cell);
+                }
+                Console.WriteLine();
+            }
+        }
+
+    }
+}
