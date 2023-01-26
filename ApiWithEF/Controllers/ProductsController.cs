544fff98340219ab9524634e85d8967f1c4cb763
Ivan <v.gritskevich25@gmail.com>
2023-01-26 17:20:32 +0300

    added api

---
+++ ApiWithEF/Controllers/ProductsController.cs
@@ -0,0 +1,43 @@
+﻿using ApiWithEF.Models;
+using ApiWithEF.Persistance;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiWithEF.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreDbContext _context;
+
+        public ProductsController(StoreDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> GetAllAsync()
+        {
+            return await _context.Products.ToListAsync();
+        }
+
+        [HttpPost("name/{name}/price/{price}")]
+        public async Task<IActionResult> AddProductAsync(string name, decimal price)
+        {
+            var product = new Product
+            {
+                Name = name,
+                Price = price
+            };
+
+            await _context.AddAsync(product);
+
+            // SaveChanges возвращает количество изменённых строк, т.к. под капотом это обычный insert
+            // при успешном выполнении он должен записать одну строку
+            var linesCount = await _context.SaveChangesAsync();
+
+            return Ok(linesCount == 1);
+        }
+    }
+}
