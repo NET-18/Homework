b4455bbd22860267908620d2c8f5a8a18e08f29f
Simon <arseni.ustinovich@gmail.com>
2023-02-01 23:16:54 +0300

    hw added

---
+++ arseni-ustinovich_25.01.2023/HW25.01.2023/Persistance/WeatherForecastDbContext.cs
@@ -0,0 +1,21 @@
+﻿using HW25._01._2023.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW25._01._2023.Persistance
+{
+    public class WeatherForecastDbContext : DbContext
+    {
+        public DbSet<WeatherForecast> weatherForecasts { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {
+            base.OnConfiguring(builder);
+
+            builder.UseSqlServer(@"Data Source=User-PC;Initial Catalog=WF_Database;Trusted_Connection=True;TrustServerCertificate=True");
+        }
+    }
+}
---
+++ arseni-ustinovich_25.01.2023/HW25.01.2023/Program.cs
@@ -0,0 +1,21 @@
+﻿using HW25._01._2023.Models;
+using HW25._01._2023.Persistance;
+using Newtonsoft.Json;
+using System;
+
+namespace HW25._01._2023
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using var context = new WeatherForecastDbContext();
+            var master = new WeatherForecastDbHandler(context);
+
+            ConsoleHelper.Init(master);
+            ConsoleHelper.StartInfo();
+
+            while (!ConsoleHelper.ReadCommand()){ }
+        }
+    }
+}
