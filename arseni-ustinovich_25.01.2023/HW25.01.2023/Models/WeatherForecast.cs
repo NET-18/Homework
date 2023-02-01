d5caabd69f95979cfd1ef5e86462a8e61be48c8e
Simon <arseni.ustinovich@gmail.com>
2023-02-01 23:02:31 +0300

    added 25.01 hw

---
+++ arseni-ustinovich_25.01.2023/HW25.01.2023/Models/WeatherForecast.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW25._01._2023.Models
+{
+    public class WeatherForecast
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+
+        public int TempC { get; set; }
+
+        public int TempF => 32 + (int)(TempC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}
+
