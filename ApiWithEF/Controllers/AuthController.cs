e8971a954a017071289f417c5788beaf23a3a666
Andrey_m4k <andrey.mar4uk2011@yandex.ru>
2023-02-25 21:00:52 +0300

    initial

---
+++ ApiWithEF/Controllers/AuthController.cs
@@ -0,0 +1,23 @@
+﻿using AutorisationApi.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AutorisationApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly TokenService _tokenService;
+
+        public AuthController(TokenService tokenService)
+        {
+            _tokenService = tokenService;
+        }
+        [HttpPost("singinin/{username}")]
+        public ActionResult<string> SignIn(string username)
+        {
+            return _tokenService.CreateToken(username);
+        }
+    }
+}
